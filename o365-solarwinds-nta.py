a8e7bd84af6c4f3874e0ace41fa824e736926462
Story <Ben.Story@HSHS.ORG>
2019-08-14 10:16:30 -0500

    Create main function.

--- o365-solarwinds-nta.py
+++ o365-solarwinds-nta.py
@@ -34,8 +34,6 @@ def printXML(endpointSets):
         print('IPv4 Firewall IP Address Ranges')
         #print (flatIps)
         currentServiceArea = " "
-
-
         output.write ("<AddressGroups xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://tempuri.org/IPAddressGroupsSchema.xsd\">\n")
         for ip in flatIps:
             serviceArea = ip [0]
@@ -57,38 +55,41 @@ def printXML(endpointSets):
         # TODO send mail (e.g. with smtplib/email modules) with new endpoints data
 
 
+def main (argv):
 
+    # path where client ID and latest version number will be stored
+    datapath = 'endpoints_clientid_latestversion.txt'
+    # fetch client ID and version if data exists; otherwise create new file
+    if os.path.exists(datapath):
+        with open(datapath, 'r') as fin:
+            clientRequestId = fin.readline().strip()
+            latestVersion = fin.readline().strip()
+    else:
+        clientRequestId = str(uuid.uuid4())
+        latestVersion = '0000000000'
+        with open(datapath, 'w') as fout:
+            fout.write(clientRequestId + '\n' + latestVersion)
+    version = webApiGet('version', 'Worldwide', clientRequestId)
+    if version['latest'] > latestVersion:
+        print('New version of Office 365 worldwide commercial service instance endpoints detected')
+        # write the new version number to the data file
+        with open(datapath, 'w') as fout:
+            fout.write(clientRequestId + '\n' + version['latest'])
+        # invoke endpoints method to get the new data
+        endpointSets = webApiGet('endpoints', 'Worldwide', clientRequestId)
+        # filter results for Allow and Optimize endpoints, and transform these into tuples with port and category
+        flatUrls = []
+        for endpointSet in endpointSets:
+            if endpointSet['category'] in ('Optimize', 'Allow'):
+                category = endpointSet['category']
+                urls = endpointSet['urls'] if 'urls' in endpointSet else []
+                tcpPorts = endpointSet['tcpPorts'] if 'tcpPorts' in endpointSet else ''
+                udpPorts = endpointSet['udpPorts'] if 'udpPorts' in endpointSet else ''
+                flatUrls.extend([(category, url, tcpPorts, udpPorts) for url in urls])
+        flatIps = []
+        printXML(endpointSets)
+    else:
+        print('Office 365 worldwide commercial service instance endpoints are up-to-date')
 
-# path where client ID and latest version number will be stored
-datapath = 'endpoints_clientid_latestversion.txt'
-# fetch client ID and version if data exists; otherwise create new file
-if os.path.exists(datapath):
-    with open(datapath, 'r') as fin:
-        clientRequestId = fin.readline().strip()
-        latestVersion = fin.readline().strip()
-else:
-    clientRequestId = str(uuid.uuid4())
-    latestVersion = '0000000000'
-    with open(datapath, 'w') as fout:
-        fout.write(clientRequestId + '\n' + latestVersion)
-version = webApiGet('version', 'Worldwide', clientRequestId)
-if version['latest'] > latestVersion:
-    print('New version of Office 365 worldwide commercial service instance endpoints detected')
-    # write the new version number to the data file
-    with open(datapath, 'w') as fout:
-        fout.write(clientRequestId + '\n' + version['latest'])
-    # invoke endpoints method to get the new data
-    endpointSets = webApiGet('endpoints', 'Worldwide', clientRequestId)
-    # filter results for Allow and Optimize endpoints, and transform these into tuples with port and category
-    flatUrls = []
-    for endpointSet in endpointSets:
-        if endpointSet['category'] in ('Optimize', 'Allow'):
-            category = endpointSet['category']
-            urls = endpointSet['urls'] if 'urls' in endpointSet else []
-            tcpPorts = endpointSet['tcpPorts'] if 'tcpPorts' in endpointSet else ''
-            udpPorts = endpointSet['udpPorts'] if 'udpPorts' in endpointSet else ''
-            flatUrls.extend([(category, url, tcpPorts, udpPorts) for url in urls])
-    flatIps = []
-    printXML(endpointSets)
-else:
-    print('Office 365 worldwide commercial service instance endpoints are up-to-date')
+if __name__ == '__main__':
+    main(sys.argv)
